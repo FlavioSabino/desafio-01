63e4e1a6a1b2238af49fadcd37eec75350e9bc03
FlavioSabino <s.flavio10@hotmail.com>
2021-08-15 16:53:34 -0300

    criando componente TaskItem

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { View,TouchableOpacity, Text, Image, StyleSheet } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { View,TouchableOpacity, Text, Image, StyleSheet, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import trashIcon from '../assets/icons/trash/trash.png'
+import editIcon from '../assets/edit/edit.png'
 import { EditTaskArgs } from '../Páginas/Home';
 import { Task } from './TasksList';
 
@@ -14,9 +15,37 @@ interface TaskItemProps {
 }
 
 export function TaskItem({task, editTask, removeTask, toggleTaskDone}: TaskItemProps) {
+  const [isEditing, setIsEditing] = useState(false)
+  const [taskNewTitleValue, setTaskNewTitleValue] = useState(task.title)
+  const textInputRef = useRef<TextInput>(null)
+
+  function handleStartEditiong(){
+    setIsEditing(true)
+  }
+
+  function handleCancelEditing(){
+    setTaskNewTitleValue(task.title)
+    setIsEditing(false)
+  }
+
+  function handleSubmitEditing(){
+    editTask({ taskId: task.id, taskNewTitle: taskNewTitleValue})
+    setIsEditing(false)
+  }
+
+  useEffect(() =>{
+    if (textInputRef.current){
+      if (isEditing){
+        textInputRef.current.focus()
+      } else{
+        textInputRef.current.blur()
+      }
+    }
+  },[isEditing])
+
   return (
-    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}} >
-     <View>
+    <View style={styles.container} >
+     <View style={styles.infoContainer} >
        <TouchableOpacity
           activeOpacity={0.7}
           style={styles.taskButton}
@@ -33,30 +62,58 @@ export function TaskItem({task, editTask, removeTask, toggleTaskDone}: TaskItemP
             />
             )}
           </View>
-
-           <Text  
+      
+          <TextInput
+            value={taskNewTitleValue}
+            onChangeText={setTaskNewTitleValue}
+            editable={isEditing}
+            onSubmitEditing={handleSubmitEditing}
             style={task.done ? styles.taskTextDone : styles.taskText}
-              >
-              {task.title}
-            </Text>
+            ref={textInputRef}
+          />
              </TouchableOpacity>
             </View>
-
-         <TouchableOpacity            
-            style={{ paddingHorizontal: 24 }}
-            onPress = {() => removeTask(task.id)}
+        
+         <View style={styles.icosContainer} >
+           {isEditing ? (
+             <TouchableOpacity  
+                onPress = { handleCancelEditing}
+             >
+                <Icon name="x" size={24} color="#ffff" />
+          </TouchableOpacity>
+           ) : (
+              <TouchableOpacity            
+                 onPress = {handleStartEditiong}
             >
-           <Image source={trashIcon} style={{tintColor:"#ffff" }} />
-            </TouchableOpacity>  
+                <Image source={editIcon} style={{tintColor:"#ffff" }} />
+         </TouchableOpacity>
+           )}
+            <View style={styles.iconsDivider} />
+
+            <TouchableOpacity            
+               onPress = {() => removeTask(task.id)}
+               disabled={isEditing}
+            >
+           <Image source={trashIcon} style={{ opacity: isEditing ? 0.2 : 1, tintColor:"#ffff" }} />
+         </TouchableOpacity>
+         </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container:{
+    flex:1,
+     flexDirection: 'row',
+      alignItems: 'center', 
+      justifyContent: 'space-between'
+  },
+  infoContainer:{
+    flex:1
+  },
   taskButton: {
     flex: 1,
-    paddingHorizontal: 24,
-    paddingVertical: 15,
+    paddingHorizontal: 24,    
     marginBottom: 4,
     borderRadius: 4,
     flexDirection: 'row',
@@ -67,7 +124,7 @@ const styles = StyleSheet.create({
     width: 16,
     borderRadius: 4,
     borderWidth: 1,
-    borderColor: '#B2B2B2',
+    borderColor: '#ffff',
     marginRight: 15,
     alignItems: 'center',
     justifyContent: 'center'
@@ -89,5 +146,17 @@ const styles = StyleSheet.create({
     color: '#1DB863',
     textDecorationLine: 'line-through',
     fontFamily: 'Inter-Medium'
+  },
+  icosContainer:{
+    flexDirection:'row',
+    alignItems:'center',
+    paddingLeft:12,
+    paddingRight:24
+  },
+  iconsDivider:{
+    width:1,
+    height:24,
+    backgroundColor:'#fff',
+    marginHorizontal:12
   }
 })
